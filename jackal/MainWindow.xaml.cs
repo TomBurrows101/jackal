6f575d37c8ae1dff428f6f07fa7c5d7edf521bf6
Tom.Burrows101 <tom.burrowskb@gmail.com>
2023-06-26 20:29:51 +0100

    Added nav, added continue text

--- jackal/MainWindow.xaml.cs
+++ jackal/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace jackal
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
